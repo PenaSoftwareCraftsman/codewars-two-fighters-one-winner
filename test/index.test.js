ede176781dc327cc36887b2029573b55a987873c
PenaSoftwareCraftsman <jose.henrique.pena.goulart@gmail.com>
2023-07-13 20:58:01 -0300

    test: testing if Figther have damagePerAttack

--- test/index.test.js
+++ test/index.test.js
@@ -12,4 +12,10 @@ describe('Fighters', () => {
 
     expect(fighter.health).toBe(2);
     });
+
+    it('Should return a 2 when a fighter is created with ("Jose", 2, 2)', () => {
+        const fighter = new Fighter("Jose", 2, 2);
+
+    expect(fighter.damagePerAttack).toBe(2);
+    });
 });
